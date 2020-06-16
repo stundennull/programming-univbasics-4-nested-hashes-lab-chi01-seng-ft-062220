ee3cf17504ef7c36de92de25cacd3b84c6c801b9
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 08:31:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash[:alan_kay][:known_for] = alan_kay_is_known_for
 
 puts programmer_hash
 end
