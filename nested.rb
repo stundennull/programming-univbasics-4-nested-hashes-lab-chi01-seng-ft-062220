a4b8e835df1a5a45665a1a935e4904b3bb748803
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 08:03:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,7 +56,6 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:alan_kay][:known_for]
 
 end
 
