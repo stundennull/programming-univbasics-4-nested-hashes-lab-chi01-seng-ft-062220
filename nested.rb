7a475d3cd5d3903e779748475d50ad680ed96228
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-16 08:15:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby", :languages => ["Lisp", "C"] }
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"] }
   
  puts programmer_hash
 
